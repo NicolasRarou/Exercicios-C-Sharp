0dbcbc0304193de02e4b4a6a8cc51b42abf8426a
Nicolas Einstein <92048614+NicolasRarou@users.noreply.github.com>
2022-05-20 02:58:37 -0300

    Add files via upload

---
+++ ExercicioBásico1_TrocaDeValoresDeVariáveis.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Training
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Mudando o valor de uma variavél
+
+            // string
+           /*  String x = "Training";
+             String y = "Complete";
+             string z = x;
+
+             Console.WriteLine(x + ' ' + y);
+
+             x = y;
+             y = z;
+             Console.WriteLine(x + ' ' +  y );
+
+             Console.ReadLine(); */
+
+            // double
+            double a = 5.9;
+            double b = 6.6;
+            double c = a;
+
+            Console.WriteLine(a + " e " + b);
+
+            a = b;
+            b = c;
+
+            Console.WriteLine(a + " e " + b);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
